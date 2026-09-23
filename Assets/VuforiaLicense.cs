5b53c05e796ec9872afc568a1ddcff6d54f51140
gui-cafe <guiparis07@gmail.com>
2026-09-23 09:40:27 -0300

    configurado pra build

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVRrWWhlci8vLy8vQUFBQm1WcWEveWJ5YVU4eHB1S09qUWFqeDA5S3Myd3ViUU1xMnBaQlRkQUF1WTZOcUxyU1F2SHlhMHd2TWlaMWRsMU8vTzRpUHA1UXZFeDRkaXdScHZZMXNxNy9XM25FMldUTnJGS3ZxVDBaUWI3VjhNU3MwVEdBcUtKUktSUllUVXh6NTl0NWxEWUVsbXlvYjhXeVhOdXJpNGYwOWdWMXhXT24ycUJKUGk3Tm51a2x1eVJsbFBGanhEbkUydkcvVGc0NTdLTzBZVGRMek1Jb0NkKzJ6VGdscXVEekFZYUw3Mm1ucEM0cUkzdEltY2NSYWRDbExyR1RvUmNtMXZ0bmgvV2hScG1jcVVFTTFzd21TRlBWRkVmbTVJWlg3Y2p3NmY2NkNoQ3duYk9RS00vbkxmdStqQkFPN2hzUFVYYThEUUdTNnVwdFdJWFVSM0lsSWx4Wmhtbk1aMUFpRUk3UGllNE1uQWhsY1ZFMDJ0S1k=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
